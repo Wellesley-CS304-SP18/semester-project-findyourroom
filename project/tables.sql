11d8b350d4e1c8ca008e4d0ae2dad3a3afa88184
mmuchaku <mmuchaku@wellesley.edu>
2018-04-29 22:51:00 -0400

    Update tables.sql

--- project/tables.sql
+++ project/tables.sql
@@ -1,12 +1,21 @@
 drop table if exists review;
 create table review(
-	RID int(1),
-	roomNum int(3),
-	pro boolean,
-	con boolean, 
-	comment varchar(100),
+	RID int(1) not null,
+	roomNum int(3) not null,
+	pro tinyint(1),
+	con tinyint(1), 
+	comment varchar(100) not null,
 	foreign key (RID) references dormInfo(RID),
 	foreign key (roomNum) references roomInfo(roomNumber)
 );
-	
+
+drop table if exists photo;
+create table photo(
+	RID int(1) not null,
+	roomNum int(3) not null,
+	length int(5) not null, 
+	path varchar(100) not null,
+	foreign key (RID) references dormInfo(RID),
+	foreign key (roomNum) references roomInfo(roomNumber)
+);
 	
