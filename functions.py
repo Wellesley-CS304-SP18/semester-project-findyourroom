0fb10a785a1b1d34bc347e1a03a60385e671902b
reneehuang <rhuang@wellesley.edu>
2018-05-01 18:01:33 -0400

    updated functions for sql queries to match our table updates

--- functions.py
+++ functions.py
@@ -49,23 +49,23 @@ import dbconn2
 def roomExists(conn, dormID, roomNumber):
     '''Execute SQL statement to check if the room exists'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('SELECT * FROM roomInfo WHERE dormID=%s AND roomNumber=%s',[dormID, roomNumber])
+    curs.execute('SELECT * FROM room WHERE dormID=%s AND roomNumber=%s',[dormID, roomNumber])
     return curs.fetchone()
     
 #make sure to compute avgRating before putting in argument 
-def updateAvgRating(conn, RID, RoomNum, avgRating):
+def updateAvgRating(conn, dormID, RoomNum, avgRating):
     '''Execute SQL statement to update avgRating'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('INSERT INTO movie VALUES avgRating=%s WHERE RID=%s AND RoomNum = %s',[avgRating,RID,RoomNum])
+    curs.execute('INSERT INTO movie VALUES avgRating=%s WHERE dormID=%s AND roomNumber = %s',[avgRating,RID,RoomNum])
  
 #check if the table associated with the room exists already 
-def reviewExists(conn, RID, RoomNum, review, pros, cons):
+def reviewExists(conn, dormID, roomNum, review, pros, cons):
     '''Execute SQL statement to check if the review for that room exists'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('SELECT * FROM review WHERE RID=%s AND RoomNum=%s',[RID,RoomNum])
+    curs.execute('SELECT * FROM review WHERE RID=%s AND RoomNum=%s',[dormID,roomNum])
     return curs.fetchone()
 
-def updateReview(conn, RID, RoomNum, review):
+def updateReview(conn, dormID, roomNum, review):
     '''Execute SQL statement to add Review'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('INSERT INTO review VAlUES review=%s,pros=%s,cons=%s WHERE RID=%s AND RoomNum = %s',[review, pros, cons, RID, RoomNum])
@@ -74,15 +74,21 @@ def updateReview(conn, RID, RoomNum, review):
 def addPhotos(conn, RID, RoomNum, size, path):
     '''Execute SQL statement to update images associated with the room'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('INSERT INTO pic VALUES size = %s, path = %s, WHERE RID=%s AND RoomNum = %s',[size, path, RID, RoomNum]) 
+    curs.execute('INSERT INTO pic VALUES size = %s, path = %s, WHERE dormID=%s AND roomNumber = %s',[size, path, RID, RoomNum]) 
 
 #currently only filtering based on res hall name and average rating. 
-def getListofRooms(conn, RID, rating):
+def getListofRooms(conn, dormID, rating):
     '''Execute SQL statement to get all the list of rooms based on user preference'''
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
-    curs.execute('SELECT * from roomInfo WHERE RID=%s AND avgRating = %s',[RID, rating])
+    curs.execute('SELECT * from room WHERE dormID=%s AND avgRating = %s',[dormID, rating])
     return curs.fetchall
-    
+
+def getListOfDorms(conn):
+    '''Execute SQL statement to get list of all dorms'''
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('SELECT * from dorm WHERE dormID=%s',[dormID])
+    return curs.fetchall
+
 #need to have more argument for user to filter /if so probably need to add more boolean to each dorm such as east/west side, has dinning hall, has gym etc
 #alpha version : delete comment 
 
